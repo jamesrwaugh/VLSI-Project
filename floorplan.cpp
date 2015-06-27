3a72bb35180501034eb509c23561f7680e0d35d3
TehPwns <p00n3dj002@yahoo.com>
2015-06-26 21:25:06 -0400

    Added base floorplanning files

---
+++ floorplan.cpp
@@ -0,0 +1,7 @@
+#include "floorplan.h"
+
+std::string floorplan(const module &partition)
+{
+    (void)(partition);
+    return "Not Implemented Yet!";
+}
---
+++ floorplan.h
@@ -0,0 +1,12 @@
+#ifndef FLOORPLAN_H
+#define FLOORPLAN_H
+#include "module.h"
+
+/* VLSI Floorplanning Implementation
+ * Input: A module to perform floorplanning on
+ * Output: A polish expression representing an efficient slicing
+ *  floorplan placement */
+
+std::string floorplan(const module& partition);
+
+#endif
